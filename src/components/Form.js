022d8a1d81285282133be5ba6f48230c515ef6e8
x4wiz <mail@23u.ru>
2021-06-22 10:14:20 +0800

    Initial

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Form() {
+	return (
+		<div>
+			<h2>Form goes here</h2>
+		</div>
+	);
+}
+
+export default Form;
---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Todos() {
+	return (
+		<div>
+			<h3>Todos list here</h3>
+		</div>
+	);
+}
+
+export default Todos;
